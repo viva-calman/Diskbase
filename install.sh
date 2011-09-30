a607498fcf90906a9c78a8452e253da879a54337
Viva Calman <viva.calman@gmail.com>
2011-09-30 13:39:35 +0400

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 echo "Enter site name (without http://):"
 read SITENAME
-echo "Enter server IP"
-read SERVIP
 echo "Enter path to storage directory:"
 read STORAGE
 echo "Enter admin password:"
 read ADMINPASS
-echo "Enter server ip"
+echo "Enter samba server IP"
 read SERVADDR
 echo
 echo
@@ -26,13 +24,13 @@ echo admindisk:$ADMINPASS | chpasswd
 (echo $ADMINPASS;echo $ADMINPASS)|smbpasswd -a -s admindisk
 echo
 echo "Set up domain name in scripts"
-STOR=\\$STORAGE
+STOR=$(echo $STORAGE|sed -e 's/\//\\\//g')
 FILES=$(ls ./www/cgi-bin)
-sed -i -e "s/http:\/\/localhost/$SITENAME/" ./www/index.html
+sed -i -e "s/http:\/\/localhost/http:\/\/$SITENAME/" ./www/index.html
 for I in $FILES
 do
 	echo -n .
-     sed -i -e "s/http:\/\/localhost/$SITENAME/" ./www/cgi-bin/$I
+     sed -i -e "s/http:\/\/localhost/http:\/\/$SITENAME/" ./www/cgi-bin/$I
 done
 echo
 echo "Update shell scripts."
@@ -46,9 +44,9 @@ sed -i -e "s/STORAGE\=/STORAGE\=$STOR/" ./www/cgi-bin/mount.sh
 echo -n .
 sed -i -e "s/SDIR\=/SDIR\=$STOR/" ./www/cgi-bin/delete.sh
 echo -n .
-sed -i -e "/##SERVERADDRESS##/$SERVADDR/" ./www/cgi-bin/reg.sh
+sed -i -e "s/##SERVERADDRESS##/$SERVADDR/" ./www/cgi-bin/reg.sh
 echo -n .
-sed -i -e "/##SERVERADDRESS##/$SERVADDR/" ./www/cgi-bin/getbat.pl
+sed -i -e "s/##SERVERADDRESS##/$SERVADDR/" ./www/cgi-bin/getbat.pl
 echo -n .
 
 echo
